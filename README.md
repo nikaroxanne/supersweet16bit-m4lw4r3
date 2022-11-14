62aeed6ea343d292ed4b04b574ae08959e692f96
Nika Korchok <nikakw@gmail.com>
2022-11-14 01:46:21 -0500

    added more detailed description to helloworld.asm comments, updated README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # supersweet16bit-m4lw4r3
+Sample COM Programs, Tools for Reverse Engineering and Analysis of MS-DOS malware
--- helloworld.asm
+++ helloworld.asm
@@ -2,7 +2,9 @@
 .MODEL TINY
 
 ;******************************************************************************
-;	Sample COM Program that prints "Hello, MS-DOS" to stdout 
+;	Template asm file 
+;	used for developing test COM programs for MS-DOS malware analysis 
+;	Sample COM Program that prints "Hello, world!" to stdout 
 ;	To be used in DOSBOX (or similar) MS-DOS Emulator program 
 ;	Must be compiled with link16.exe (MASM32 preferably) 
 ;
@@ -20,7 +22,7 @@ _hello	PROC	NEAR
 	mov	dx,offset a$msg
 	int	21h
 	
-	
+	;;terminate program 	
 	mov	ax,4C00h
 	int	21h
 
@@ -30,5 +32,4 @@ a$msg	db	'Hello, MS-DOS!',0Dh,0Ah,24h
 ;;message to display to stdout
 
 ;;msg_len	equ	$-msg
-;;CODE	ends
 	end	_hello
