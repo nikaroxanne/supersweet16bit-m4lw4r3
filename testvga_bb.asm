916de308afcab5d94a375c8ee4c10a2789409924
Nika Korchok <nikakw@gmail.com>
2022-11-20 21:35:55 -0500

    working version of test_vga with vga animation on keypress; fixes typos for registers from previous commit, adds improved function signatures

--- testvga_bb.asm
+++ testvga_bb.asm
@@ -18,32 +18,56 @@
 	org 100h
 
 _start	PROC	NEAR
-	mov	ah,40h
-	mov	bx,1
+	;;mov	ah,40h
+	;;mov	bx,1
 	;;stdout == 1
-	mov	dx,offset a$msg
-	int	21h
+	;;mov	dx,offset a$msg
+	;;int	21h
 
-	mov	bx,0B800h
+	mov	ax,0B800h
 	mov	es,ax
 	mov	di,0h
 	mov	cx,0h
 	jmp	short crash
 
 crash:
-	add	di,031Dh
+	add	di,051Dh
 	cmp	di,3E80h
-	jl	short crash_n
+	jl	near ptr crash_n
 	sub	di,3E80h
+
+;******************************************************************************
+;
+;;copies new pixel values to VGA buffer 
+;
+;******************************************************************************
 
 crash_n:
 	mov	al,es:[di]
 	add	ax,di
 	mov	es:[di],al
-	inc	cx
-	cmp	cx,65556h
-	jl 	short crash
-	ret
+
+	
+;******************************************************************************
+;
+;	Reads char from buffer (function 0h,int16h)
+; 	Char returned in al
+; 	If char in al == 0x1b (ESC) then terminate program
+;	Else, continue VGA *~pretty picture~* loop
+;
+;******************************************************************************
+	mov	ah,0h
+	int	16h
+	cmp	al, 01Bh
+	jnz	near ptr crash
+	
+
+
+;;
+;;	inc	cx
+;;	cmp	cx,65556h
+;;	jl 	short crash
+;;	ret
 
 	
 	mov	ax,4C00h
@@ -51,7 +75,7 @@ crash_n:
 
 _start	ENDP
 
-a$msg	db	'Hello, MS-DOS!',0Dh,0Ah,24h
+;;a$msg	db	'Hello, MS-DOS!',0Dh,0Ah,24h
 ;;message to display to stdout
 
 ;;msg_len	equ	$-msg
